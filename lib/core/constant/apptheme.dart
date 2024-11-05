e2ef6ba204dc597b4ad19ae70286b910b64d2242
azab <mohamedahmedelazab@yahoo.com>
2024-11-05 14:20:58 +0200

    new

--- lib/core/constant/apptheme.dart
+++ lib/core/constant/apptheme.dart
@@ -1,38 +1,58 @@
-
 import 'package:suezproduction/core/constant/color.dart';
 import 'package:flutter/material.dart';
 
 ThemeData themeEnglish = ThemeData(
   fontFamily: "Roboto",
   textTheme: const TextTheme(
-      headline1: TextStyle(
-          fontWeight: FontWeight.bold, fontSize: 22, color: AppColor.black),
-      headline2: TextStyle(
-          fontWeight: FontWeight.bold, fontSize: 26, color: AppColor.black),
-      bodyText1: TextStyle(
-          height: 2,
-          color: AppColor.kBlueColor,
-          fontWeight: FontWeight.bold,
-          fontSize: 14),
-      bodyText2: TextStyle(height: 2, color: AppColor.grey, fontSize: 14),),
+    displayLarge: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 26, // Adjust font size according to your design
+      color: AppColor.black,
+    ),
+    displayMedium: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 22, // Adjust font size according to your design
+      color: AppColor.black,
+    ),
+    bodyLarge: TextStyle(
+      height: 2,
+      color: AppColor.kBlueColor,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    bodyMedium: TextStyle(
+      height: 2,
+      color: AppColor.grey,
+      fontSize: 14,
+    ),
+  ),
   primarySwatch: Colors.blue,
 );
 
 ThemeData themeArabic = ThemeData(
-
   fontFamily: "Tajawal",
   textTheme: const TextTheme(
-
-      headline1: TextStyle(
-          fontWeight: FontWeight.bold, fontSize: 22, color: AppColor.black),
-      headline2: TextStyle(
-          fontWeight: FontWeight.bold, fontSize: 26, color: AppColor.black),
-      bodyText1: TextStyle(
-          height: 2,
-          color: AppColor.kBlueColor,
-          fontWeight: FontWeight.bold,
-          fontSize: 14),
-      bodyText2: TextStyle(height: 2, color: AppColor.grey, fontSize: 14)),
+    displayLarge: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 26, // Adjust font size according to your design
+      color: AppColor.black,
+    ),
+    displayMedium: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 22, // Adjust font size according to your design
+      color: AppColor.black,
+    ),
+    bodyLarge: TextStyle(
+      height: 2,
+      color: AppColor.kBlueColor,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    ),
+    bodyMedium: TextStyle(
+      height: 2,
+      color: AppColor.grey,
+      fontSize: 14,
+    ),
+  ),
   primarySwatch: Colors.blue,
-
 );
